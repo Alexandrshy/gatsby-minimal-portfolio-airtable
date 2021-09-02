5c248ac7541c7c0e79ab1cf4d88b6769c6f7e05e
Alexandrshy <alexandrshy@gmail.com>
2021-09-02 23:41:37 +0400

    feat: added header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import { Logo } from "../logo"
+import { Nav } from "../nav"
 
 import * as s from "./style.module.css"
 
 type PropsType = {}
 
 export const Header: React.FC<PropsType> = () => {
-  return <header className={s.header}></header>
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <Logo />
+        <div>
+          <h1 className={s.title}>
+            <Link to="/" className={s.link}>
+              Alex Shulaev
+            </Link>
+          </h1>
+          <Nav />
+        </div>
+      </div>
+    </header>
+  )
 }
