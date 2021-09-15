f34d74181eef48fc70cab09d53cc2416a10e59a6
Alexandrshy <alexandrshy@gmail.com>
2021-09-15 23:46:07 +0400

    feat: added title componrnt

---
+++ src/components/title/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import * as s from "./style.module.css"
+
+type PropsType = {
+  subtitle: string
+}
+
+export const Title: React.FC<PropsType> = ({ children, subtitle }) => (
+  <>
+    <h3 className={s.subtitle}>{subtitle}</h3>
+    <h4 className={s.title}>{children}</h4>
+  </>
+)
