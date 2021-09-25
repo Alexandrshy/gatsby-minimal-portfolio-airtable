593301f840730be7016108e713138222f3a4610d
Alexandrshy <alexandrshy@gmail.com>
2021-09-26 01:05:31 +0400

    fix: changed title component

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -3,12 +3,12 @@ import React from "react"
 import * as s from "./style.module.css"
 
 type PropsType = {
-  subtitle: string
+  subtitle?: string
 }
 
 export const Title: React.FC<PropsType> = ({ children, subtitle }) => (
   <>
-    <h3 className={s.subtitle}>{subtitle}</h3>
+    {subtitle && <h3 className={s.subtitle}>{subtitle}</h3>}
     <h4 className={s.title}>{children}</h4>
   </>
 )
