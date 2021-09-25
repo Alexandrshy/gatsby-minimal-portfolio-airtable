e23ac36cd0eeda1b1e44663204ce3f94617eb93a
Alexandrshy <alexandrshy@gmail.com>
2021-09-26 01:04:04 +0400

    feat: added blog component

---
+++ src/components/blog/index.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+import { Title } from "../title"
+import { Publications } from "../publications"
+
+import type { AirtableBlogType } from "../../types/tables"
+
+export const Blog: React.FC = () => {
+  const {
+    allAirtable: { nodes },
+  } = useStaticQuery<AirtableBlogType>(graphql`
+    {
+      allAirtable(
+        filter: { table: { eq: "blog" } }
+        sort: { order: DESC, fields: data___date }
+      ) {
+        nodes {
+          recordId
+          table
+          data {
+            short_description
+            title
+            description {
+              childMarkdownRemark {
+                html
+                timeToRead
+              }
+            }
+            status
+            slug
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Title>Publications</Title>
+      {nodes.length ? (
+        <Publications list={nodes} path={``} />
+      ) : (
+        <p>
+          Oops... No posts have been written yet, come back later when I get
+          some inspiration
+        </p>
+      )}
+    </>
+  )
+}
