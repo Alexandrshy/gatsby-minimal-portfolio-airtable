f7c867d972b5aa749e8a4e903ce4d562045a9cc8
Alexandrshy <alexandrshy@gmail.com>
2021-09-26 00:35:57 +0400

    fix: renamed types

--- src/types/tables.tsx
+++ src/types/tables.tsx
@@ -1,52 +1,52 @@
 import { IGatsbyImageData } from "gatsby-plugin-image"
 
-export type AirtableBlog = {
-  allAirtable: {
-    nodes: [
-      {
-        recordId: string
-        data: {
-          short_description: string
-          title: string
-          description: {
-            childMarkdownRemark: {
-              html: string
-              timeToRead: number
-            }
-          }
-          status: "Published" | "Draft"
-          slug: string
-          date: string
-        }
+export type BlogNodesType = {
+  recordId: string
+  table: string
+  data: {
+    short_description: string
+    title: string
+    description: {
+      childMarkdownRemark: {
+        html: string
+        timeToRead: number
       }
-    ]
+    }
+    status: "Published" | "Draft"
+    slug: string
+    date: string
   }
 }
 
-export type AirtableProjects = {
-  allAirtable: {
-    nodes: [
-      {
-        recordId: string
-        data: {
-          short_description: string
-          title: string
-          description: {
-            childMarkdownRemark: {
-              html: string
-              timeToRead: number
-            }
-          }
-          cover: {
-            localFiles: [
-              { childImageSharp: { gatsbyImageData: IGatsbyImageData } }
-            ]
-          }
-          status: "Published" | "Draft"
-          slug: string
-          date: string
-        }
+export type ProjectsNodesType = {
+  recordId: string
+  table: string
+  data: {
+    short_description: string
+    title: string
+    description: {
+      childMarkdownRemark: {
+        html: string
+        timeToRead: number
       }
-    ]
+    }
+    cover: {
+      localFiles: [{ childImageSharp: { gatsbyImageData: IGatsbyImageData } }]
+    }
+    status: "Published" | "Draft"
+    slug: string
+    date: string
+  }
+}
+
+export type AirtableBlogType = {
+  allAirtable: {
+    nodes: BlogNodesType[]
+  }
+}
+
+export type AirtableProjectsType = {
+  allAirtable: {
+    nodes: ProjectsNodesType[]
   }
 }
