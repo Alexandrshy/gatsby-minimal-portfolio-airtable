7cf1fecfdd10d024df0f4fb6be6fef0d4069a0a0
Alexandrshy <alexandrshy@gmail.com>
2021-09-25 23:33:46 +0400

    feat: added SEO component for HP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,49 @@
 import React from "react"
+import SEO from "react-seo-component"
 
 import { Description } from "../components/description"
 import { Chapter } from "../components/chapter"
 import { ShortBlog } from "../components/shortBlog"
 import { ShortProjects } from "../components/shortProjects"
 
+import { useSiteMetadata } from "../hooks/useSiteMetadata"
+
 import * as s from "./index.module.css"
 
-export default () => (
-  <div className={s.wrapper}>
-    <Chapter>
-      <Description />
-    </Chapter>
-    <Chapter>
-      <ShortBlog />
-    </Chapter>
-    <Chapter>
-      <ShortProjects />
-    </Chapter>
-  </div>
-)
+export default () => {
+  const {
+    title,
+    titleTemplate,
+    description,
+    siteLanguage,
+    siteLocale,
+    twitterUsername,
+  } = useSiteMetadata()
+
+  return (
+    <>
+      <SEO
+        title={title}
+        titleSeparator="|"
+        titleTemplate={titleTemplate}
+        description={description}
+        pathname="/"
+        siteLanguage={siteLanguage}
+        siteLocale={siteLocale}
+        twitterUsername={twitterUsername}
+        article
+      />
+      <div className={s.wrapper}>
+        <Chapter>
+          <Description />
+        </Chapter>
+        <Chapter>
+          <ShortBlog />
+        </Chapter>
+        <Chapter>
+          <ShortProjects />
+        </Chapter>
+      </div>
+    </>
+  )
+}
