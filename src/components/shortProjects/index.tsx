80fe468cb0a5d96c9a9dab38ca2bbc8c2d92b5fd
Alexandrshy <alexandrshy@gmail.com>
2021-09-26 01:04:48 +0400

    fix: fixed type and path

--- src/components/shortProjects/index.tsx
+++ src/components/shortProjects/index.tsx
@@ -4,12 +4,12 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 import { Title } from "../title"
 
-import type { AirtableProjects } from "../../types/tables"
+import type { AirtableProjectsType } from "../../types/tables"
 
 import * as s from "./style.module.css"
 
 export const ShortProjects: React.FC = () => {
-  const data = useStaticQuery<AirtableProjects>(graphql`
+  const data = useStaticQuery<AirtableProjectsType>(graphql`
     {
       allAirtable(
         filter: { table: { eq: "projects" } }
@@ -18,6 +18,7 @@ export const ShortProjects: React.FC = () => {
       ) {
         nodes {
           recordId
+          table
           data {
             title
             slug
@@ -43,9 +44,9 @@ export const ShortProjects: React.FC = () => {
     <>
       <Title subtitle="projects">My Open Source projects</Title>
       <ul className={s.list}>
-        {data.allAirtable.nodes.map(({ recordId, data }) => (
+        {data.allAirtable.nodes.map(({ recordId, data, table }) => (
           <li key={recordId} className={s.item}>
-            <Link to={data.slug} className={s.link}>
+            <Link to={`${table}/${data.slug}`} className={s.link}>
               <GatsbyImage
                 image={data.cover.localFiles[0].childImageSharp.gatsbyImageData}
                 alt={data.title}
