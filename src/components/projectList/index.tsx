0022ab81a6669c1958780a10205e4de92c24c080
Alexandrshy <alexandrshy@gmail.com>
2021-10-01 23:08:24 +0400

    fix: fixed type for component

--- src/components/projectList/index.tsx
+++ src/components/projectList/index.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 import type { ProjectsNodesType } from "../../types/tables"
@@ -8,21 +7,20 @@ import * as s from "./style.module.css"
 
 type PropsType = {
   list: ProjectsNodesType[]
-  path: string
 }
 
-export const ProjectList: React.FC<PropsType> = ({ list, path }) => (
+export const ProjectList: React.FC<PropsType> = ({ list }) => (
   <ul className={s.list}>
     {list.map(({ recordId, data }) => (
       <li key={recordId} className={s.item}>
-        <Link to={`${path}${data.slug}`} className={s.link}>
+        <a href={data.link} className={s.link} target="_blank" rel="noopener">
           <GatsbyImage
             image={data.cover.localFiles[0].childImageSharp.gatsbyImageData}
             alt={data.title}
             className={s.img}
           />
           <h5 className={s.title}>{data.title}</h5>
-        </Link>
+        </a>
       </li>
     ))}
   </ul>
