b726b97ab4046a8d4346bf6076ca0bb379ce55ff
Alexandrshy <alexandrshy@gmail.com>
2021-10-01 23:47:02 +0400

    fix: fixed type

--- src/types/tables.tsx
+++ src/types/tables.tsx
@@ -1,21 +1,23 @@
 import { IGatsbyImageData } from "gatsby-plugin-image"
 
+export type BlogDataType = {
+  short_description: string
+  title: string
+  description: {
+    childMarkdownRemark: {
+      html: string
+      timeToRead: number
+    }
+  }
+  status: "Published" | "Draft"
+  slug: string
+  date: string
+}
+
 export type BlogNodesType = {
   recordId: string
   table: string
-  data: {
-    short_description: string
-    title: string
-    description: {
-      childMarkdownRemark: {
-        html: string
-        timeToRead: number
-      }
-    }
-    status: "Published" | "Draft"
-    slug: string
-    date: string
-  }
+  data: BlogDataType
 }
 
 export type ProjectsNodesType = {
